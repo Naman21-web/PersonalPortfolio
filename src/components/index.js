ad0d484058f5d6ef951d0020a3a2573d39eb84d5
Naman Jain <jainn0984@gmail.com>
2026-06-02 00:06:15 +0530

    Added index file

---
+++ src/components/index.js
@@ -0,0 +1,37 @@
+import { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas } from "./canvas";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Projects from "./Projects";
+import ProjectsPage from "./ProjectsPage";
+// import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import Works from "./Works";
+import WorkPage from './WorkPage';
+import ContactPage from './ContactPage';
+import MatrixRain from './MatrixRain';
+import TechVisualization from './TechVisualization';
+import Education from './Education';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Projects,
+  ProjectsPage,
+  Contact,
+  Works,
+  BallCanvas, 
+  ComputersCanvas, 
+  EarthCanvas, 
+  StarsCanvas,
+  WorkPage,
+  ContactPage,
+  MatrixRain,
+  TechVisualization,
+  Education
+};
